2b8defccded4a8039939ac5dcbf8a92ccf195155
Anirudh Menon <thenasaplusit@proton.me>
2026-06-03 23:17:38 +0200

    Add initial logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,127 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final TextEditingController _CAScontroller = TextEditingController();
+  final TextEditingController _MTcontroller = TextEditingController();
+  final TextEditingController _buscontroller = TextEditingController();
+  final TextEditingController _channelcontroller = TextEditingController();
+
+  double? cas = 0;
+  double? MT = 0;
+  double? bus = 0;
+  double? channels = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RAMSpeed',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
         brightness: Brightness.dark,
+        primaryColor: Colors.blueAccent,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('RAMSpeed'),
           backgroundColor: Colors.blueAccent,
         ),
-        body: Center(
+        body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(24.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Text(
+                  "Enter the following data:",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 16),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      "Enter the following data:",
-                      style: TextStyle(fontSize: 24),
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: "Speed (MT/s)",
+                        ),
+                        controller: _MTcontroller,
+                      ),
                     ),
-                    Row(
-                      children: [
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: "Speed in MT/s",
-                          ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: "Bus width",
                         ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: "Bus width",
-                          ),
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: "Number of channels",
-                          ),
-                        ),
-                      ],
+                        controller: _buscontroller,
+                      ),
                     ),
-                    Text(
-                      "To calculate the latency, enter the CL- rating (e.g. CL30)",
-                    ),
-                    Row(
-                      children: [
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: "CL rating",
-                          ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: "Channels",
                         ),
-                      ],
+                        controller: _channelcontroller,
+                      ),
                     ),
                   ],
+                ),
+
+                const SizedBox(height: 32),
+
+                const Text(
+                  "To calculate the latency, enter the CL rating (e.g., 30):",
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: "CL rating",
+                        ),
+                        controller: _CAScontroller,
+                      ),
+                    ),
+                    const Expanded(flex: 2, child: SizedBox()),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      cas = double.tryParse(_CAScontroller.text);
+                      MT = double.tryParse(_MTcontroller.text);
+                      bus = double.tryParse(_buscontroller.text);
+                      channels = double.tryParse(_channelcontroller.text);
+                    });
+                  },
                 ),
               ],
             ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.info),
+          onPressed: () {
+            print('TBD');
+          },
         ),
       ),
     );
